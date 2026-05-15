b49d91418da6e5e3bba7ce1afe1f2dec3ab8d258
Shaik Subhani <ssk41262@gmail.com>
2026-05-15 15:49:08 +0530

    fix: switch to Tailwind v3 for Versal compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  plugins: [
-    react(),
-    
-  ],
+  plugins: [react()],  // no tailwindcss plugin needed for v3
 })
